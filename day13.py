b2f9baed9c15315a417b2d71c7e17994cc485ce6
Abdullah Kashif <abdullahmk800@gmail.com>
2025-08-19 12:39:08 +0500

    update

--- day13.py
+++ day13.py
@@ -1,5 +1,12 @@
+import json
+import os
 
-Student = {
+if os.path.exists("store.txt"):
+    with open("store.txt","r") as f:
+        Student=json.load(f)
+else:
+
+ Student = {
     "2803": {
         "name": "Abdullah",
         "marks": {
@@ -9,7 +16,7 @@ Student = {
         },
         "GPA": 3.7,
     },
-    "2867": {
+    "2804": {
         "name": "Ali",
         "marks": {
             "Math": 58,
@@ -38,17 +45,21 @@ Student = {
 # }
 # print("New Student added")
 # print(Student[student_id])
+def save_data():
+    with open("store.txt","w") as f:
+        f.write(json.dumps(Student,indent=4))
 
 
 while True:
     print("STUDENT REPORT CARD SYSTEM")
     print("1:Add Student")
-    print("2:View Student")
-    print("3:Delete Student")
-    print("4:Exit")
+    print("2:Update Student")
+    print("3:View Student")
+    print("4:Delete Student")
+    print("5:Exit")
     opinion = int(input("Enter your opinion="))
 
-    # ADD
+                              # ADD
     if opinion == 1:
         student_id = input("Enter the id=")
         name = input("Enter the name=")
@@ -69,44 +80,57 @@ while True:
        }
         print("New Student added")
         print(Student[student_id])
-        import json
-
-        f = open("store.txt", "w")
-        f.write(json.dumps(Student, indent=4))
-        f.close()
-
         
-                                #   view
+        save_data()
+
+                               # Update
     elif opinion ==2:
+        std_id=input(("Enter the student id="))
+        if std_id in Student:
+            print("New data",Student[std_id])
+
+            name=input("Enter the new name(press s to keep the same):")
+            Math=input("Enter the new Math(press s to keep the same):")
+            Eng=input("Enter the new Eng(press s to keep the same):")
+            Urdu=input("Enter the new Urdu(press s to keep the same):")
+
+            if name.strip():
+                Student[std_id]["name"]=name
+            if Math.strip():
+                Student[std_id]["marks"]["Math"]=int(Math)
+            if Eng.strip():
+                Student[std_id]["marks"]["Eng"]=int(Eng)
+            if Urdu.strip():
+                Student[std_id]["marks"]["Urdu"]=int(Urdu)            
+
+            marks=Student[std_id]["marks"]    
+            avg=(marks["Math"]+marks["Eng"]+marks["Urdu"])/3
+            GPA=avg/25
+            Student[std_id]["GPA"]=round(GPA,2)
+            print("Student updated successfully")
+            print(Student[std_id])
+
+
+            save_data()
+
+                                #   View
+    elif opinion ==3:
         std_id=input(("Enter the Studennt ID="))
         if std_id in Student:
             print(Student[std_id])
         else:
             print("Student is not in Report")
-                            #  delete
-    elif opinion ==3:
+                            #  Delete
+    elif opinion ==4:
         std_id=input("Enter ID to delete=")
         if std_id in Student:
             del Student[std_id]
             print("Student deleted successfully")        
         else:
             print("Student not deleted")
-                            #   exit  
-    elif opinion==4: 
+                            #   Exit  
+    elif opinion==5: 
         break       
                  
     else:
         print("nothing is found")
-# a=(input("Ask  for student id="))
-# if a in Student:
-#     print(Student[a])
-# else:
-#     print("Student not found")
-
-# with open("store.txt", "w") as f:
-#     json.dump(Student, f)
-
-# with open("store.txt", "r") as f:
-#     c = f.read()
-#     print("store.txt")
-#     print(c)
---
+++ day15.py
@@ -0,0 +1,27 @@
+# print("Add water")
+# print("add tea leaf")
+# print("add sugar and milk")
+# print("stir and wait till it boil")
+# print("pour into cups")
+
+# balance=1000
+# amount=int(input("enter amount="))
+
+# if balance>=amount:
+#     balance -=amount
+#     print("withdraw successfully")
+#     print("Remaining balance is",balance)
+# else:
+#     print("not enough balance")
+#     print("your account has ",balance) 
+
+
+
+username="Fahad"
+password=12234
+input_username=input("Enter username=")
+input_password=int(input("Enter a password="))
+if username==input_username and password==input_password:
+    print("login")
+else:
+    print("Try again")    
